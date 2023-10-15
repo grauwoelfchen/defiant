0426cb0c8948c350b18d63673bfdb8a93596becf
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-10-16 06:07:22 +0900

    Change temporary column names in user_id migration

--- src/defiant-web/migration/20231015001930_add_user_id_to_bookmarks/up.sql
+++ src/defiant-web/migration/20231015001930_add_user_id_to_bookmarks/up.sql
@@ -4,23 +4,23 @@ ALTER TABLE bookmarks
   -- bookmarks_user_id_fkey
   ADD COLUMN user_id BIGINT REFERENCES users (id) MATCH FULL NOT NULL
   -- data rotation (https://wiki.postgresql.org/wiki/Alter_column_position)
-, ADD COLUMN tmp_uuid UUID NOT NULL DEFAULT uuid_generate_v4()
-, ADD COLUMN tmp_url CHARACTER VARYING(2048) NOT NULL
-, ADD COLUMN tmp_title CHARACTER VARYING(256) NULL
-, ADD COLUMN tmp_description TEXT NULL
-, ADD COLUMN tmp_created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+, ADD COLUMN _uuid UUID NOT NULL DEFAULT uuid_generate_v4()
+, ADD COLUMN _url CHARACTER VARYING(2048) NOT NULL
+, ADD COLUMN _title CHARACTER VARYING(256) NULL
+, ADD COLUMN _description TEXT NULL
+, ADD COLUMN _created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
     DEFAULT (now() AT TIME ZONE 'utc')
-, ADD COLUMN tmp_updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+, ADD COLUMN _updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
     DEFAULT (now() AT TIME ZONE 'utc')
 ;
 
 UPDATE bookmarks SET
-  tmp_uuid=uuid
-, tmp_url=url
-, tmp_title=title
-, tmp_description=description
-, tmp_created_at=created_at
-, tmp_updated_at=updated_at
+  _uuid=uuid
+, _url=url
+, _title=title
+, _description=description
+, _created_at=created_at
+, _updated_at=updated_at
 ;
 
 ALTER TABLE bookmarks
@@ -32,12 +32,12 @@ ALTER TABLE bookmarks
 , DROP COLUMN updated_at CASCADE
 ;
 
-ALTER TABLE bookmarks RENAME COLUMN tmp_uuid to uuid;
-ALTER TABLE bookmarks RENAME COLUMN tmp_url to url;
-ALTER TABLE bookmarks RENAME COLUMN tmp_title to title;
-ALTER TABLE bookmarks RENAME COLUMN tmp_description to description;
-ALTER TABLE bookmarks RENAME COLUMN tmp_created_at to created_at;
-ALTER TABLE bookmarks RENAME COLUMN tmp_updated_at to updated_at;
+ALTER TABLE bookmarks RENAME COLUMN _uuid to uuid;
+ALTER TABLE bookmarks RENAME COLUMN _url to url;
+ALTER TABLE bookmarks RENAME COLUMN _title to title;
+ALTER TABLE bookmarks RENAME COLUMN _description to description;
+ALTER TABLE bookmarks RENAME COLUMN _created_at to created_at;
+ALTER TABLE bookmarks RENAME COLUMN _updated_at to updated_at;
 
 -- recreate missing one
 CREATE UNIQUE INDEX bookmarks_uuid_idx ON bookmarks(uuid);
